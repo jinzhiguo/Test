11d161f307fb5e9f890bcd73f73708cb68320ebe
jerry <jerry_guoguo@sina.com>
2018-04-13 14:13:20 +0800

    添加了一行注解

--- springBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ springBootDemo/src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
-
+	//入口类
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
